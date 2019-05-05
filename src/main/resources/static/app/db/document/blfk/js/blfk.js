06034953b0c22b65e33213e3a174617ce81a3ea1
lixue <lixue@lixue>
2019-05-05 13:54:37 +0800

    办理反馈表格功能完善

--- src/main/resources/static/app/db/document/blfk/js/blfk.js
+++ src/main/resources/static/app/db/document/blfk/js/blfk.js
@@ -5,6 +5,7 @@ var userUrl = {"url":"/app/db/document/grdb/data/userTree.json","dataType":"text
 var leftMenuUrl = {"url":"/app/db/document/blfk/data/leftMenu.json","dataType":"text"};//左侧菜单
 var leaderId=getUrlParam("menuid")||"";//代理领导
 var grid = null;
+var grid2 = null;
 var total=0;//列表中，数据的总条数
 var pageModule = function(){
 	var initgrid = function(){
@@ -62,7 +63,70 @@ var pageModule = function(){
             overflowx:true,
             pagesize: 15,
             pageyno:true,
-            paramobj:{search:$("#searchVal").val(),docStatus:$("input[name='documentStatus']:checked").val()},
+            paramobj:{search:$("#searchVal").val(),docStatus:$("input[name='documentStatus']:checked").val(),menuId:$("#classType li.active").attr("id")},
+            loadafter:function(data){
+            	total=data.total;
+            },
+            url: tableList
+       });
+	}
+	
+	var initgrid2 = function(){
+        grid2 = $("#gridcont2").createGrid({
+            columns:[
+            	{display:"军委办件号",name:"",width:"8%",align:"left",title:true,render:function(rowdata,n){
+               	 	return rowdata.banjianNumber;
+                }},
+                {display:"办理状态",name:"",width:"6%",align:"center",render:function(rowdata,n){
+                	var statusName="";
+               	 	var bgColor="";
+               	 	if(rowdata.status==1){
+	               	 	statusName="办理中";
+	               		bgColor="#2BAA81";
+            	 	}else if(rowdata.status==2){
+	               	 	statusName="已办结";
+	               		bgColor="#BCBCBC";
+               	 	}else if(rowdata.status==3){
+	               	 	statusName="常态落实";
+	               	 	bgColor="#BCBCBC";
+               	 	}
+  				  	return '<div title="'+statusName+'" class="btn btn-xs btn-color" style="background-color:'+bgColor+';">'+statusName+'</div>';
+                 }},
+                 {display:"办件标题",name:"",width:"15%",align:"left",render:function(rowdata){
+                	 var cuiban="";
+                 	 if(rowdata.cuibanFlag=="1"){
+                 		 cuiban = '<label class="cuibanlabel">催办</label>';
+                	 }
+                	 return '<a title="'+rowdata.docTitle+'" class="table-title" href="../../djlr/html/djlr_view.html?fileId='+rowdata.id+'" target="iframe1">'+cuiban+rowdata.docTitle+'</a>'
+                 }},
+                 {display:"工作指示内容",name:"",width:"20%",align:"left",paixu:false,title:true,render:function(rowdata){
+                	 return '';
+                 }},
+                 {display:"督办落实情况",name:"",width:"20%",align:"left",paixu:false,title:true,render:function(rowdata){
+                	 var duban="";
+                 	 if(rowdata.cuibanFlag=="0"){
+                 		duban = '<label class="cuibanlabel">已更新</label>';
+                	 }
+                	 return duban+'';
+                 }},
+                 {display:"承办单位/人",name:"",width:"11%",align:"left",paixu:false,title:true,render:function(rowdata){
+               		return '';
+                 }},
+                 {display:"转办时间",name:"",width:"10%",align:"center",render:function(rowdata){
+                	 return rowdata.firstZbTime.substring(0,16); 
+                 }},
+                 {display:"最新反馈时间",name:"",width:"10%",align:"center",paixu:false,render:function(rowdata){
+                	
+                 }}
+            ],
+            width:"100%",
+            height:"100%",
+            checkbox: true,
+            rownumberyon:true,
+            overflowx:true,
+            pagesize: 15,
+            pageyno:true,
+            paramobj:{search:$("#searchVal").val(),docStatus:$("input[name='documentStatus']:checked").val(),menuId:$("#classType li.active").attr("id")},
             loadafter:function(data){
             	total=data.total;
             },
@@ -77,13 +141,22 @@ var pageModule = function(){
 			success:function(data){
 				$("#classType").html("");
 				$.each(data,function(i,item){
-					$("#classType").append('<li class="'+(i==0?"active":"")+'" id="'+item.id+'"><span>'+item.text+'</span><font>('+item.num+')</font><i class="fa fa-angle-right"></i></li>');
+					$("#classType").append('<li class="'+(i==0?"active":"")+'" data_flag="'+item.gridflag+'" id="'+item.id+'"><span>'+item.text+'</span><font>('+item.num+')</font><i class="fa fa-angle-right"></i></li>');
 				});
 				
 				$("#classType li").click(function(){
 					$(this).siblings().removeClass("active");
 					$(this).addClass("active");
-					refreshgrid();
+					if($(this).attr("data_flag") == "1"){
+						$("#gridcont").show();
+						$("#gridcont2").hide();
+						refreshgrid1();
+					}else{
+						$("#gridcont2").show();
+						$("#gridcont").hide();
+						refreshgrid2();
+					}
+					
 				});
 			}
 		});	
@@ -230,6 +303,7 @@ var pageModule = function(){
 		initControl:function(){
 			leftMenufn();
 			initgrid();
+			initgrid2();
 			numsListfn();
 			initother();
 			inittree();
@@ -239,13 +313,15 @@ var pageModule = function(){
 			numsListfn();
 		},
 		initgrid2:function(){
-			initgrid();
+			initgrid2();
+			numsListfn();
 		}
 	};
 }();
 //查询
-function refreshgrid(){
-	var search = $("#searchVal").val();
-	grid.setparams({search:search,documentStatus:$("input[name='documentStatus']:checked").val(),menuId:$("#classType li.active").attr("id")});
-	grid.loadtable();
+function refreshgrid1(){
+	pageModule.initgrid();
+}
+function refreshgrid2(){
+	pageModule.initgrid2();
 }
