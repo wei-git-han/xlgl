b2a9a69b013b0fe421cffac5a3513c9898df4b30
zst <zst@css.com.cn>
2019-05-29 19:17:54 +0800

    bug修复

--- src/main/java/com/css/app/db/business/service/impl/ImportExcleServiceImpl.java
+++ src/main/java/com/css/app/db/business/service/impl/ImportExcleServiceImpl.java
@@ -396,12 +396,12 @@ public class ImportExcleServiceImpl implements ImportExcleService {
 					String[] detailUndertakes = undertake.split("\\|");
 					if (detailUndertakes != null && detailUndertakes.length > 0) {
 						undertakeMap.put("cbdw", detailUndertakes[0]);// 承办单位；
-						if (StringUtils.equals("\\*", detailUndertakes[1])) {
+						if (StringUtils.equals("*", detailUndertakes[1])) {
 							undertakeMap.put("cbr", "");// 承办人；
 						} else {
 							undertakeMap.put("cbr", detailUndertakes[1]);// 承办人；
 						}
-						if (StringUtils.equals("\\*", detailUndertakes[2])) {
+						if (StringUtils.equals("*", detailUndertakes[2])) {
 							undertakeMap.put("cbrdh", "");// 承办人；
 						} else {
 							undertakeMap.put("cbrdh", detailUndertakes[2]);// 承办人电话
