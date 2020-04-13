096d39608d7c5f5a79cedbab977f1061d2e74a96
hanwei <hanwei>
2020-04-13 14:36:43 +0800

    督办批量导入

--- src/main/java/com/css/app/db/business/controller/DocumentSzpsController.java
+++ src/main/java/com/css/app/db/business/controller/DocumentSzpsController.java
@@ -1,10 +1,18 @@
 package com.css.app.db.business.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.css.base.utils.DateUtil;
+import com.css.addbase.SysOrgan;
+import com.css.addbase.apporgan.service.BaseAppUserService;
+import com.css.app.db.config.entity.RoleSet;
+import com.css.app.db.config.service.RoleSetService;
+import com.css.app.db.util.DbDefined;
+import com.css.base.utils.*;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +21,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSONObject;
 import com.css.app.db.business.entity.DocumentSzps;
 import com.css.app.db.business.service.DocumentSzpsService;
-import com.css.base.utils.Response;
-import com.css.base.utils.StringUtils;
-import com.css.base.utils.UUIDUtils;
 
 
 /**
@@ -30,6 +35,10 @@ import com.css.base.utils.UUIDUtils;
 public class DocumentSzpsController {
 	@Autowired
 	private DocumentSzpsService documentSzpsService;
+	@Autowired
+	private BaseAppUserService baseAppUserService;
+	@Autowired
+	private RoleSetService roleSetService;
 	
 	/**
 	 * 列表
@@ -61,6 +70,70 @@ public class DocumentSzpsController {
 		}
 		json.put("result", "success");
 		Response.json(json);
+	}
+
+	/**
+	 * 针对批量导入，直接粘贴复制内容
+	 */
+	@ResponseBody
+	@RequestMapping("/newSaves")
+	public void saveInfos(String leaderComment,String infoId){
+		Map<String, Object> map = new HashMap<>();
+		map.put("roleFlag", DbDefined.ROLE_1);
+		List<RoleSet> roleSetList = roleSetService.queryList(map);
+		if (StringUtils.isNotBlank(leaderComment)) {
+			String[] infomentions = leaderComment.split(",");
+			if (infomentions != null) {
+				for (int i = 0; i < infomentions.length; i++) {
+					Date date = new Date();
+					SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy");
+					String year = simpleDateFormat.format(date).substring(0, 4);
+					String strs = infomentions[i];
+					String[] contents = strs.split("：");
+					String[] contentss = strs.split(":");
+					if(contents.length == contentss.length){
+						contents = contents;
+					}else if(contents.length > contentss.length){
+						contents = contents;
+					}else {
+						contents = contentss;
+					}
+					if (contents != null) {
+						String userId = "";
+						int t = contents[0].indexOf("部长");
+						String userName = contents[0].substring(0, t + 2);
+						for (int j = 0; j < roleSetList.size(); j++) {
+							//判断这个人是否是首长
+							if (StringUtils.equals(userName, roleSetList.get(j).getUserName())) {
+								userId = roleSetList.get(j).getUserId();
+								if (StringUtils.isNotBlank(contents[1])) {//批示内容不为空的才保存。
+									String pishiContent = contents[1].substring(1, contents[1].length() - 1);
+									int tMonth = contents[0].indexOf("月");
+									String month = contents[0].substring(t + 2, tMonth);
+									if (Integer.parseInt(month) < 10) {
+										month = 0 + month;
+									}
+									int tDay = contents[0].indexOf("日");
+									String day = contents[0].substring(tMonth + 1, tDay);
+									if (Integer.parseInt(day) < 10) {
+										day = 0 + day;
+									}
+									DocumentSzps documentSzps = new DocumentSzps();
+									documentSzps.setId(UUIDUtils.random());
+									documentSzps.setUserId(userId);
+									documentSzps.setUserName(userName);
+									documentSzps.setLeaderComment(pishiContent);
+									documentSzps.setCreatedTime(year + "年" + month + "月" + day + "日");
+									documentSzps.setInfoId(infoId);
+									documentSzpsService.save(documentSzps);
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
 	}
 
 	/**
