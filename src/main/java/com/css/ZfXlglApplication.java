9e80184ff6e1e3b025b891fd62d5687640a22569
hanwei <hanwei>
2020-07-28 10:29:51 +0800

    修改启动类名字

---
+++ src/main/java/com/css/ZfXlglApplication.java
@@ -0,0 +1,29 @@
+package com.css;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+/**
+ * war包和jar包通用配置
+ * jar包使用bootRepackage命令：gradlew -b build.gradle bootRepackage
+ * war包使用war命令：gradlew -b build-war.gradle war
+ */
+@SpringBootApplication
+@ServletComponentScan
+@EnableEurekaClient
+@EnableScheduling
+@EnableTransactionManagement
+public class ZfXlglApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(ZfXlglApplication.class);
+	    }
+	public static void main(String[] args) {
+		SpringApplication.run(ZfXlglApplication.class, args);
+	}
+}
