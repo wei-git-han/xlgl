bae7209d6edc08cb3a28856d9d9a22179ffaaae7
hanwei <hanwei>
2020-08-04 16:21:59 +0800

    菜单权限

---
+++ src/main/java/com/css/app/xlgl/service/impl/DocumentMenuPermissionServiceImpl.java
@@ -0,0 +1,56 @@
+package com.css.app.xlgl.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.css.app.xlgl.dao.DocumentMenuPermissionDao;
+import com.css.app.xlgl.entity.DocumentMenuPermission;
+import com.css.app.xlgl.service.DocumentMenuPermissionService;
+
+import java.util.List;
+import java.util.Map;
+
+
+
+
+@Service("documentMenuPermissionService")
+public class DocumentMenuPermissionServiceImpl implements DocumentMenuPermissionService {
+	@Autowired
+	private DocumentMenuPermissionDao documentMenuPermissionDao;
+	
+	@Override
+	public DocumentMenuPermission queryObject(String id){
+		return documentMenuPermissionDao.queryObject(id);
+	}
+	
+	@Override
+	public List<DocumentMenuPermission> queryList(Map<String, Object> map){
+		return documentMenuPermissionDao.queryList(map);
+	}
+	
+	@Override
+	public void save(DocumentMenuPermission documentMenuPermission){
+		documentMenuPermissionDao.save(documentMenuPermission);
+	}
+	
+	@Override
+	public void update(DocumentMenuPermission documentMenuPermission){
+		documentMenuPermissionDao.update(documentMenuPermission);
+	}
+	
+	@Override
+	public void delete(String id){
+		documentMenuPermissionDao.delete(id);
+	}
+	
+	@Override
+	public void deleteBatch(String[] ids){
+		documentMenuPermissionDao.deleteBatch(ids);
+	}
+	
+	@Override
+	public void deleteByUserId(String uid){
+		documentMenuPermissionDao.deleteByUserId(uid);
+	}
+	
+}
