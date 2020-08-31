c1544b72a16e8eabd03cda7e33682d8d8b5ea0a7
李振楠 <1761071739>
2020-08-31 13:17:12 +0800

    修改人员管理接口

--- src/main/java/com/css/app/xlgl/controller/PeopleManagementController.java
+++ src/main/java/com/css/app/xlgl/controller/PeopleManagementController.java
@@ -1,17 +1,28 @@
 package com.css.app.xlgl.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.css.addbase.apporgan.entity.BaseAppOrgan;
 import com.css.addbase.apporgan.entity.BaseAppUser;
@@ -21,6 +32,7 @@ import com.css.addbase.apporgmapped.entity.BaseAppOrgMapped;
 import com.css.addbase.apporgmapped.service.BaseAppOrgMappedService;
 import com.css.addbase.constant.AppConstant;
 import com.css.addbase.constant.AppInterfaceConstant;
+import com.css.app.xlgl.dto.TxlUserDto;
 import com.css.app.xlgl.entity.XlglAdminSet;
 import com.css.app.xlgl.service.XlglAdminSetService;
 import com.css.base.utils.CrossDomainUtil;
@@ -47,6 +59,9 @@ public class PeopleManagementController {
 	private BaseAppUserService baseAppUserService;
 	@Autowired
 	private XlglAdminSetService adminSetService;
+	
+	@Value("${filePath}")
+	private String filePath;
 
 	/**
 	 * 
@@ -79,21 +94,27 @@ public class PeopleManagementController {
 				}
 			}
 		}
-		
-		LinkedMultiValueMap<String, Object> linkeMap = new LinkedMultiValueMap<String,Object>();
 		Map<String, Object> map = new HashMap<>();
 		PageHelper.startPage(page, limit);
 		List<BaseAppOrgan> queryList = baseAppOrganService.queryList(map);
 		PageUtils pageUtil = new PageUtils(queryList);
+		queryList = getBaseAppOrganList(queryList,queryList2,status);
+		Response.json("page",pageUtil);
+	}
+	
+	private List<BaseAppOrgan> getBaseAppOrganList(List<BaseAppOrgan> queryList,List<XlglAdminSet> queryList2,Boolean status) {
+		LinkedMultiValueMap<String, Object> linkeMap = new LinkedMultiValueMap<String,Object>();
 		for (BaseAppOrgan baseAppOrgan : queryList) {
-			if(status) {
-				String str = "0";
-				if(queryList2.size()>0) {
-					 str =baseAppOrgan.getId().equals(queryList2.get(0).getDeptId())?"1":"0";
+			if(status != null) {
+				if(status) {
+					String str = "0";
+					if(queryList2.size()>0) {
+						 str =baseAppOrgan.getId().equals(queryList2.get(0).getDeptId())?"1":"0";
+					}
+					baseAppOrgan.setStatus(str);
+				}else {
+					baseAppOrgan.setStatus("1");
 				}
-				baseAppOrgan.setStatus(str);
-			}else {
-				baseAppOrgan.setStatus("1");
 			}
 			linkeMap.add("organId", baseAppOrgan.getId());
 			JSONObject jsonData = this.getNumber(linkeMap);
@@ -120,23 +141,9 @@ public class PeopleManagementController {
 			baseAppOrgan.setSjzwrs(userIdList);
 			baseAppOrgan.setZwrate(zwRate);
 		}
-		Response.json("page",pageUtil);
+		return queryList;
 	}
 	
-	/**
-	 * 单位人员详情-鼠标悬停显示
-	 * */
-	@ResponseBody
-	@RequestMapping("/qxjUserInto")
-	public void list(String userId) {
-		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<String,Object>();
-		map.add("userId", userId);
-		// 获取办件开放的接口
-		String elecPath = baseAppOrgMappedService.getWebUrl(AppConstant.APP_QXJGL, AppInterfaceConstant.WEB_INTERFACE_QXJ_USER_INFO_QJDAYS);
-		JSONObject jsonData = CrossDomainUtil.getJsonData(elecPath,map);
-
-		Response.json(jsonData);
-	}
 	
 	/**
 	 * 单位人员统计列表
@@ -145,22 +152,17 @@ public class PeopleManagementController {
 	@RequestMapping("/qxjUserInfoList")
 	public void qxjUserInfoList(Integer page, Integer limit,String organId) {
 		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<String,Object>();
-		Map<String, Object> hashmap = new HashMap<>();
-		map.add("organId", organId);
-		hashmap.put("organId", organId);
-		PageHelper.startPage(page, limit);
+		map.add("orgid", organId);
+		map.add("page", page);
+		map.add("rows", limit);
+		/* Map<String, Object> hashmap = new HashMap<>();
+		 * hashmap.put("organId", organId);
+		 * PageHelper.startPage(page, limit);
 		List<BaseAppUser> queryListByOrganid = baseAppUserService.queryListByOrganid(hashmap);
-		PageUtils pageUtil = new PageUtils(queryListByOrganid);
-		List<String> list = getUserArray();
-		for (BaseAppUser baseAppUser : queryListByOrganid) {
-			if(list.contains(baseAppUser.getAccount())) {
-				baseAppUser.setReign("1");
-			}else {
-				baseAppUser.setReign("0");
-			}
-		}
+		PageUtils pageUtil = new PageUtils(queryListByOrganid);*/
+		JSONObject txl = getTXL(map);
 	
-		Response.json("page",pageUtil);
+		Response.json(txl);
 	}
 
 	/**
@@ -211,7 +213,12 @@ public class PeopleManagementController {
 		Response.json("baseAppOrgan",baseAppOrgan);
 	}
 	
-
+	private JSONObject getTXL(LinkedMultiValueMap<String, Object> map) {
+		// 获取清销假app的接口
+		String elecPath = baseAppOrgMappedService.getWebUrl(AppConstant.APP_TXL, AppInterfaceConstant.WEB_TXL);
+		JSONObject jsonData = CrossDomainUtil.getJsonData(elecPath,map);
+		return jsonData;
+	}
 	
 	private JSONObject getNumber(LinkedMultiValueMap<String, Object> map) {
 		// 获取清销假app的接口
@@ -244,5 +251,155 @@ public class PeopleManagementController {
 		}
 		return accountList;
 	} 
-
+	
+	/**
+	 * 单位人员详情-鼠标悬停显示 目前废弃不用
+	 * */
+	@ResponseBody
+	@RequestMapping("/qxjUserInto")
+	public void list(String userId) {
+		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<String,Object>();
+		map.add("userId", userId);
+		// 获取办件开放的接口
+		String elecPath = baseAppOrgMappedService.getWebUrl(AppConstant.APP_QXJGL, AppInterfaceConstant.WEB_INTERFACE_QXJ_USER_INFO_QJDAYS);
+		JSONObject jsonData = CrossDomainUtil.getJsonData(elecPath,map);
+		Response.json(jsonData);
+	}
+	/**
+	 * 各单位人员情况统计列表
+	 * 导出功能
+	 * */
+	@ResponseBody
+	@RequestMapping("/export")
+	public void export() {
+		List<BaseAppOrgan> queryList = baseAppOrganService.queryList(null);
+		queryList = getBaseAppOrganList(queryList,null,null);
+		File tempFile = new File(filePath, "人员管理-各单位人员情况统计表.xls");
+		if (tempFile.exists()) {
+			tempFile.delete();
+		} else {
+			tempFile.getParentFile().mkdirs();
+		}
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		InputStream is;
+		String[] title ={"序号","部门名称","应在位人数","实际在位人数","人员在位率","请假人数","休假人数"};
+		try {
+			is = createExcelList(queryList,title,tempFile.getAbsolutePath());
+			is.close();
+			resultMap.put("fileUrl", tempFile.getAbsoluteFile());
+			resultMap.put("fileName", tempFile.getName());
+			resultMap.put("result", "success");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		Response.json(resultMap);
+	}
+	/**
+	 * 各单位人员情况统计列表导出
+	 * */
+	private InputStream createExcelList(List<BaseAppOrgan> list, String[] title ,String fileName) throws Exception {
+		FileOutputStream fout = null;
+		try {
+			HSSFWorkbook wb = new HSSFWorkbook();
+			HSSFSheet sheet = wb.createSheet();
+			HSSFRow row = sheet.createRow(0);
+			for (int i = 0; i < title.length; i++) {
+				HSSFCell createCell = row.createCell(i);
+				createCell.setCellValue(title[i]);
+			}
+			for (int i = 1; i < list.size(); i++) {
+				HSSFRow rows = sheet.createRow(i);
+				HSSFCell cell0 = rows.createCell(0);//序号
+				cell0.setCellValue(i);
+				
+				HSSFCell cell1 = rows.createCell(1);//部门名称
+				cell1.setCellValue(list.get(i).getName());
+				
+				HSSFCell cell2 = rows.createCell(2);//应在位人数
+				cell2.setCellValue(list.get(i).getYzwrs());
+				
+				HSSFCell cell3 = rows.createCell(3);//实际在位人数
+				cell3.setCellValue(list.get(i).getSjzwrs());
+				
+				HSSFCell cell4 = rows.createCell(4);//人员在未率
+				cell4.setCellValue(list.get(i).getZwrate());
+				
+				HSSFCell cell5 = rows.createCell(5);//请假人数
+				cell5.setCellValue(list.get(i).getQjrs());
+				
+				HSSFCell cell6 = rows.createCell(6);//休假人数
+				cell6.setCellValue(list.get(i).getXjrs());
+			}
+			fout = new FileOutputStream(fileName);
+			wb.write(fout);
+			fout.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fout != null) {
+				fout.close();
+			}
+		}
+		return new FileInputStream(fileName);
+	}
+	
+	/**
+	 * 	organName：部门名称
+		fullname：用户名称
+		mobile：移动电话
+		post：职务
+		telephone：座机
+		address 地址（没有房间号，先用地址吧）
+		isShow：是否在位
+	 * */
+	private InputStream createExcelInfoFlie(List<TxlUserDto> list, String[] title ,String fileName) throws Exception {
+		FileOutputStream fout = null;
+		try {
+			HSSFWorkbook wb = new HSSFWorkbook();
+			HSSFSheet sheet = wb.createSheet();
+			HSSFRow row = sheet.createRow(0);
+			for (int i = 0; i < title.length; i++) {
+				HSSFCell createCell = row.createCell(i);
+				createCell.setCellValue(title[i]);
+			}
+			for (int i = 1; i < list.size(); i++) {
+				HSSFRow rows = sheet.createRow(i);
+				HSSFCell cell0 = rows.createCell(0);//用户名称
+				cell0.setCellValue(list.get(i).getFullname());
+				
+				HSSFCell cell1 = rows.createCell(1);//职务
+				cell1.setCellValue(list.get(i).getPost());
+				
+				HSSFCell cell2 = rows.createCell(2);//座机
+				cell2.setCellValue(list.get(i).getTelephone());
+				
+				HSSFCell cell3 = rows.createCell(3);//手机号
+				cell3.setCellValue(list.get(i).getMobile());
+				
+				HSSFCell cell4 = rows.createCell(4);//房间号
+				cell4.setCellValue("");
+				
+				HSSFCell cell5 = rows.createCell(5);//部门名称
+				cell5.setCellValue(list.get(i).getOrganName());
+				
+				HSSFCell cell6 = rows.createCell(6);//在位情况
+				if(list.get(i).getIsShow() != null && list.get(i).getIsShow().equals("0")) {
+					cell6.setCellValue("否");
+				}else if(list.get(i).getIsShow() != null && list.get(i).getIsShow().equals("1")) {
+					cell6.setCellValue("是");
+				}
+				
+			}
+			fout = new FileOutputStream(fileName);
+			wb.write(fout);
+			fout.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fout != null) {
+				fout.close();
+			}
+		}
+		return new FileInputStream(fileName);
+	}
 }
---
+++ src/main/java/com/css/app/xlgl/dto/TxlUserDto.java
@@ -0,0 +1,567 @@
+package com.css.app.xlgl.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 
+ * 
+ * @author 中软信息系统工程有限公司
+ * @email
+ * @date 2017-11-21 09:21:03
+ */
+public class TxlUserDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	// 用户ID
+	private String userid;
+	// 用户名字
+	private String fullname;
+	// 登录名
+	private String account;
+	// 密码
+	private String password;
+	// 性别
+	private String sex;
+	// 组织机构ID
+	private String organid;
+	// 排序ID
+	private String orderid;
+	//
+	private String dn;
+	// 是否删除
+	private String isdelete;
+	//
+	private String ca;
+	// 是否管理员
+	private Integer ismanager;
+	//
+	private String tokenid;
+	//
+	private String spid;
+	//
+	private String sn;
+	// IP地址
+	private String ip;
+	// 开始时间
+	private Date startdate;
+	// 结束时间
+	private Date enddate;
+	// 用户UUID
+	private String useruuid;
+	// 用户邮件
+	private String useremail;
+	// 密级
+	private String seclevel;
+	//
+	private Integer failedlogincount;
+	// 编辑时间
+	private Date editpwdtime;
+	// 移动电话
+	private String mobile;
+	//
+	private Long timestamp;
+	//
+	private String type;
+	// 职位
+	private String post;
+	// 座机电话
+	private String telephone;
+	// 地址
+	private String address;
+	
+	private String dept;
+	
+	// 组织机构Name
+	private String organName;
+	private String orgName;
+	// 是否收藏
+	private String isSc;
+	//是否显示
+	private String isShow;//1或空显示，0隐藏
+	//显示权限代码
+	private String rights;
+	//备注
+	private String remarks;
+	// 移动电话2
+	private String mobileTwo;
+	// 座机电话2
+	private String telephoneTwo;
+	
+	//训练管理app需要-请销假app返回
+	private JSONObject jsonData;
+
+	//
+	private String pyName;
+
+	public String getPyName() {
+		return pyName;
+	}
+
+	public void setPyName(String pyName) {
+		this.pyName = pyName;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	public String getIsSc() {
+		return isSc;
+	}
+
+	public void setIsSc(String isSc) {
+		this.isSc = isSc;
+	}
+
+	public String getOrganName() {
+		return organName;
+	}
+
+	public void setOrganName(String organName) {
+		this.organName = organName;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	/**
+	 * 设置：用户ID
+	 */
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	/**
+	 * 获取：用户ID
+	 */
+	public String getUserid() {
+		return userid;
+	}
+
+	/**
+	 * 设置：用户名字
+	 */
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	/**
+	 * 获取：用户名字
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+
+	/**
+	 * 设置：登录名
+	 */
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	/**
+	 * 获取：登录名
+	 */
+	public String getAccount() {
+		return account;
+	}
+
+	/**
+	 * 设置：密码
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * 获取：密码
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * 设置：性别
+	 */
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	/**
+	 * 获取：性别
+	 */
+	public String getSex() {
+		return sex;
+	}
+
+	/**
+	 * 设置：组织机构ID
+	 */
+	public void setOrganid(String organid) {
+		this.organid = organid;
+	}
+
+	/**
+	 * 获取：组织机构ID
+	 */
+	public String getOrganid() {
+		return organid;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setDn(String dn) {
+		this.dn = dn;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public String getDn() {
+		return dn;
+	}
+
+	public String getOrderid() {
+		return orderid;
+	}
+
+	public void setOrderid(String orderid) {
+		this.orderid = orderid;
+	}
+
+	public String getIsdelete() {
+		return isdelete;
+	}
+
+	public void setIsdelete(String isdelete) {
+		this.isdelete = isdelete;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setCa(String ca) {
+		this.ca = ca;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public String getCa() {
+		return ca;
+	}
+
+	/**
+	 * 设置：是否管理员
+	 */
+	public void setIsmanager(Integer ismanager) {
+		this.ismanager = ismanager;
+	}
+
+	/**
+	 * 获取：是否管理员
+	 */
+	public Integer getIsmanager() {
+		return ismanager;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setTokenid(String tokenid) {
+		this.tokenid = tokenid;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public String getTokenid() {
+		return tokenid;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setSpid(String spid) {
+		this.spid = spid;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public String getSpid() {
+		return spid;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setSn(String sn) {
+		this.sn = sn;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public String getSn() {
+		return sn;
+	}
+
+	/**
+	 * 设置：IP地址
+	 */
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	/**
+	 * 获取：IP地址
+	 */
+	public String getIp() {
+		return ip;
+	}
+
+	/**
+	 * 设置：开始时间
+	 */
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+
+	/**
+	 * 获取：开始时间
+	 */
+	public Date getStartdate() {
+		return startdate;
+	}
+
+	/**
+	 * 设置：结束时间
+	 */
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+
+	/**
+	 * 获取：结束时间
+	 */
+	public Date getEnddate() {
+		return enddate;
+	}
+
+	/**
+	 * 设置：用户UUID
+	 */
+	public void setUseruuid(String useruuid) {
+		this.useruuid = useruuid;
+	}
+
+	/**
+	 * 获取：用户UUID
+	 */
+	public String getUseruuid() {
+		return useruuid;
+	}
+
+	/**
+	 * 设置：用户邮件
+	 */
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+
+	/**
+	 * 获取：用户邮件
+	 */
+	public String getUseremail() {
+		return useremail;
+	}
+
+	/**
+	 * 设置：密级
+	 */
+	public void setSeclevel(String seclevel) {
+		this.seclevel = seclevel;
+	}
+
+	/**
+	 * 获取：密级
+	 */
+	public String getSeclevel() {
+		return seclevel;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setFailedlogincount(Integer failedlogincount) {
+		this.failedlogincount = failedlogincount;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public Integer getFailedlogincount() {
+		return failedlogincount;
+	}
+
+	/**
+	 * 设置：编辑时间
+	 */
+	public void setEditpwdtime(Date editpwdtime) {
+		this.editpwdtime = editpwdtime;
+	}
+
+	/**
+	 * 获取：编辑时间
+	 */
+	public Date getEditpwdtime() {
+		return editpwdtime;
+	}
+
+	/**
+	 * 设置：移动电话
+	 */
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	/**
+	 * 获取：移动电话
+	 */
+	public String getMobile() {
+		return mobile;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * 设置：
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * 获取：
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * 设置：职位
+	 */
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	/**
+	 * 获取：职位
+	 */
+	public String getPost() {
+		return post;
+	}
+
+	/**
+	 * 设置：座机电话
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	/**
+	 * 获取：座机电话
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+
+	/**
+	 * 设置：地址
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * 获取：地址
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+	public String getIsShow() {
+		return isShow;
+	}
+
+	public void setIsShow(String isShow) {
+		this.isShow = isShow;
+	}
+
+	public String getRights() {
+		return rights;
+	}
+
+	public void setRights(String rights) {
+		this.rights = rights;
+	}
+
+	public String getMobileTwo() {
+		return mobileTwo;
+	}
+
+	public void setMobileTwo(String mobileTwo) {
+		this.mobileTwo = mobileTwo;
+	}
+
+	public String getTelephoneTwo() {
+		return telephoneTwo;
+	}
+
+	public void setTelephoneTwo(String telephoneTwo) {
+		this.telephoneTwo = telephoneTwo;
+	}
+
+	public JSONObject getJsonData() {
+		return jsonData;
+	}
+
+	public void setJsonData(JSONObject jsonData) {
+		this.jsonData = jsonData;
+	}
+	
+	
+}
