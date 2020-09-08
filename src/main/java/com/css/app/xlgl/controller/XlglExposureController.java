a642a2208bd3d175bafe6c475af837f9e82c1a7e
李振楠 <1761071739>
2020-09-08 15:09:08 +0800

    修改首页-曝光台查询bug

--- src/main/java/com/css/app/xlgl/controller/XlglExposureController.java
+++ src/main/java/com/css/app/xlgl/controller/XlglExposureController.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.json.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
