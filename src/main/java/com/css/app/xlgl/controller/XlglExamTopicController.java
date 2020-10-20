f8155be538ec814b61e066f05b65afb89a84c750
李振楠 <1761071739>
2020-10-20 13:30:19 +0800

    提交测试截取视频公共类

--- src/main/java/com/css/app/xlgl/controller/XlglExamTopicController.java
+++ src/main/java/com/css/app/xlgl/controller/XlglExamTopicController.java
@@ -1,10 +1,6 @@
 package com.css.app.xlgl.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,24 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,6 +25,7 @@ import com.css.app.xlgl.entity.XlglExamFile;
 import com.css.app.xlgl.entity.XlglExamTopic;
 import com.css.app.xlgl.service.XlglExamFileService;
 import com.css.app.xlgl.service.XlglExamTopicService;
+import com.css.app.xlgl.util.CutOutVideoUtil;
 import com.css.base.utils.CurrentUser;
 import com.css.base.utils.PageUtils;
 import com.css.base.utils.Response;
@@ -141,6 +126,8 @@ public class XlglExamTopicController {
 	  * */
 	@RequestMapping("/downloadFile")
 	 public void downloadFile(String fileId) {
+	/*	String url = "C:/temp/storetemp";
+		String pictureFileid = CutOutVideoUtil.getPictureFileid(fileId, url);*/
 		HTTPFile httpFile = new HTTPFile(fileId);
 		String fileName = httpFile.getFileName();
 		Response.download(fileName, httpFile.getInputSteam());
