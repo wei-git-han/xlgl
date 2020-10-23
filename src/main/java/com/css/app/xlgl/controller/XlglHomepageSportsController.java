14435205eb5098ae74537ba2f4aa9a9e53c598c6
hanwei <hanwei>
2020-10-23 17:08:53 +0800

    体育模块接口

--- src/main/java/com/css/app/xlgl/controller/XlglHomepageSportsController.java
+++ src/main/java/com/css/app/xlgl/controller/XlglHomepageSportsController.java
@@ -1,10 +1,7 @@
 package com.css.app.xlgl.controller;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import com.css.base.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +16,6 @@ import com.css.app.xlgl.service.XlglHomepageSportsPersonService;
 import com.css.app.xlgl.service.XlglHomepageSportsService;
 import com.css.base.entity.SSOUser;
 import com.github.pagehelper.PageHelper;
-import org.thymeleaf.processor.ITextNodeProcessorMatcher;
 
 
 /**
@@ -43,49 +39,76 @@ public class XlglHomepageSportsController {
 	 */
 	@ResponseBody
 	@RequestMapping("/list")
-	public void list(Integer page, Integer limit,String type){
+	public void list(Integer page, Integer limit,String type,String updateDate,String sportsName) {
 		String userId = CurrentUser.getUserId();
 		Map<String, Object> map = new HashMap<>();
-		if(StringUtils.isNotBlank(type) && "3".equals(type)){
-			map.put("zjcg",type);//组局成功
+		if (StringUtils.isNotBlank(type) && "3".equals(type)) {
+			map.put("zjcg", type);//组局成功
 		}
-		if(StringUtils.isNotBlank(type) && "1".equals(type)){
-			map.put("bmz",type);//报名中
+		if (StringUtils.isNotBlank(type) && "1".equals(type)) {
+			map.put("bmz", type);//报名中
 		}
-		if(StringUtils.isNotBlank(type) && "2".equals(type)){
-			map.put("ybm",type);
+		if (StringUtils.isNotBlank(type) && "2".equals(type)) {
+			map.put("ybm", type);
+		}
+		if (StringUtils.isNotBlank(updateDate)) {
+			map.put("time", updateDate);
+		}
+		if (StringUtils.isNotBlank(sportsName)) {
+			map.put("projectName", sportsName);
 		}
 		PageHelper.startPage(page, limit);
 		//查询列表数据
 		List<XlglHomepageSports> xlglHomepageSportsList = xlglHomepageSportsService.queryList(map);
-		if("3".equals(type) || "2".equals(type)){//组局成功是从所有组局成功中，删除不是当前登录人的数据；已报名也是从所有数据中，删除不是当前登录人的
-		if(xlglHomepageSportsList != null && xlglHomepageSportsList.size() > 0){
-			for(int i=0;i<xlglHomepageSportsList.size();i++){
-				String sportId = xlglHomepageSportsList.get(i).getId();
-				int needNumber = xlglHomepageSportsList.get(i).getNeedNumber();
-				int haveNumber = xlglHomepageSportsList.get(i).getHaveNumber();
-				if(needNumber == haveNumber){
-					xlglHomepageSportsList.get(i).setStatus("1");
-				}else {
-					xlglHomepageSportsList.get(i).setStatus("0");
-				}
-				String creator = xlglHomepageSportsList.get(i).getCreateUser();
-				if(userId.equals(creator)){
-					xlglHomepageSportsList.get(i).setIsOpen(true);
-				}else {
-					xlglHomepageSportsList.get(i).setIsOpen(false);
-				}
-				XlglHomepageSportsPerson xlglHomepageSportsPerson = xlglHomepageSportsPersonService.queryByUserAndSportId(sportId,userId);
-				if(xlglHomepageSportsPerson == null){
-					xlglHomepageSportsList.remove(i);
-				}else {
-					xlglHomepageSportsList.get(i).setType(true);
+		if ("3".equals(type) || "2".equals(type)) {//组局成功是从所有组局成功中，删除不是当前登录人的数据；已报名也是从所有数据中，删除不是当前登录人的
+			if (xlglHomepageSportsList != null && xlglHomepageSportsList.size() > 0) {
+				for (int i = 0; i < xlglHomepageSportsList.size(); i++) {
+					String sportId = xlglHomepageSportsList.get(i).getId();
+					int needNumber = xlglHomepageSportsList.get(i).getNeedNumber();
+					int haveNumber = xlglHomepageSportsList.get(i).getHaveNumber();
+					if (needNumber == haveNumber) {
+						xlglHomepageSportsList.get(i).setStatus("1");
+					} else {
+						xlglHomepageSportsList.get(i).setStatus("0");
+					}
+
+					XlglHomepageSportsPerson xlglHomepageSportsPerson = xlglHomepageSportsPersonService.queryByUserAndSportId(sportId, userId);
+					if (xlglHomepageSportsPerson == null) {
+						xlglHomepageSportsList.remove(i);
+					} else {
+						xlglHomepageSportsList.get(i).setType(true);
+					}
 				}
 			}
 		}
+
+		for (XlglHomepageSports xlglHomepageSports : xlglHomepageSportsList) {
+			String sportId = xlglHomepageSports.getId();
+			XlglHomepageSportsPerson xlglHomepageSportsPerson = xlglHomepageSportsPersonService.queryByUserAndSportId(sportId, userId);
+			if (xlglHomepageSportsPerson != null) {
+				xlglHomepageSports.setType(true);
+			}
+			String creator = xlglHomepageSports.getUpdateUser();
+			if (userId.equals(creator)) {
+				xlglHomepageSports.setIsOpen(true);
+			} else {
+				xlglHomepageSports.setIsOpen(false);
+			}
+			List list = new ArrayList();
+			String people = xlglHomepageSports.getPeoples();
+			if(StringUtils.isNotBlank(people)){
+				String[] peoples = people.split(",");
+				if(peoples != null && peoples.length > 0){
+					for(int i = 0;i<peoples.length;i++){
+						list.add(peoples[i]);
+					}
+				}
+			}
+
+			xlglHomepageSports.setAllPeople(list);
+			PageUtils pageUtil = new PageUtils(xlglHomepageSportsList);
+			Response.json("page", pageUtil);
 		}
-		PageUtils pageUtil = new PageUtils(xlglHomepageSportsList);
-		Response.json("page",pageUtil);
 	}
 	
 	
@@ -112,7 +135,7 @@ public class XlglHomepageSportsController {
 	@ResponseBody
 	@RequestMapping("/saveOrUpdate")
 	public void saveOrUpdate(XlglHomepageSports xlglHomepageSports){
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		String curDay = format.format(new Date());
 		JSONObject jsonObject = new JSONObject();
 		SSOUser ssoUser = CurrentUser.getSSOUser();
@@ -129,11 +152,10 @@ public class XlglHomepageSportsController {
 			xlglHomepageSportsService.update(xlglHomepageSports);
 		}else {
 			xlglHomepageSports.setId(UUIDUtils.random());
-			xlglHomepageSports.setCreateUser(ssoUser.getUserId());
+			xlglHomepageSports.setCreateUser(CurrentUser.getUsername());
 			xlglHomepageSports.setCreateDate(curDay);
-			xlglHomepageSports.setUpdateUser(ssoUser.getUserId());
+			xlglHomepageSports.setUpdateUser(CurrentUser.getUserId());
 			xlglHomepageSports.setUpdateDate(curDay);
-			xlglHomepageSports.setCreateUser(ssoUser.getUserId());
 			xlglHomepageSports.setStatus("0");
 			xlglHomepageSports.setType(false);
 			xlglHomepageSports.setOrgName(CurrentUser.getOrgName());
@@ -173,8 +195,13 @@ public class XlglHomepageSportsController {
 	 */
 	@ResponseBody
 	@RequestMapping("/cacle")
-	public void cacle(String id){
-		xlglHomepageSportsService.cacle(id);
+	public void cacle(String sportId){
+		xlglHomepageSportsService.cacle(sportId);
+		XlglHomepageSports xlglHomepageSports = xlglHomepageSportsService.queryObject(sportId);
+		if(xlglHomepageSports != null){
+			xlglHomepageSports.setStatus("2");
+		}
+		xlglHomepageSportsService.update(xlglHomepageSports);
 		Response.json("result","success");
 	}
 
