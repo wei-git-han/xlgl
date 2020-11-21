f7432cde10d4261b718a2ce1db25462340b607dc
sunzh <824562329@qq.com>
2020-11-21 17:51:39 +0800

    bug修改

--- src/main/java/com/css/app/xlgl/service/impl/SurveyQuestionCountServiceImpl.java
+++ src/main/java/com/css/app/xlgl/service/impl/SurveyQuestionCountServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 
-@Service("surveyQuestionService")
+@Service("SurveyQuestionCountService")
 public class SurveyQuestionCountServiceImpl implements SurveyQuestionCountService {
 	@Autowired
 	private SurveyQuestionCountDao surveyQuestionCountDao;
