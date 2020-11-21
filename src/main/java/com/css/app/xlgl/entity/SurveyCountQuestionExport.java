70af2f6321287af20db20fcfb78459c1e45419a4
wangjinyong <hello@world.com>
2020-11-21 21:27:24 +0800

    统计实体类提交

---
+++ src/main/java/com/css/app/xlgl/entity/SurveyCountQuestionExport.java
@@ -0,0 +1,48 @@
+package com.css.app.xlgl.entity;
+
+import java.util.Date;
+
+public class SurveyCountQuestionExport {
+	private static final long serialVersionUID = 1L;
+	
+	// 题干名称
+	private String questionContent;
+	// 选项名称
+	private String optionContent;
+	// 票数
+	private String countNum;
+	// 百分比
+	private String countPercentage;
+	
+	public String getQuestionContent() {
+		return questionContent;
+	}
+
+	public void setQuestionContent(String questionContent) {
+		this.questionContent = questionContent;
+	}
+
+	public String getOptionContent() {
+		return optionContent;
+	}
+
+	public void setOptionContent(String optionContent) {
+		this.optionContent = optionContent;
+	}
+
+	public String getCountNum() {
+		return countNum;
+	}
+
+	public void setCountNum(String countNum) {
+		this.countNum = countNum;
+	}
+	
+	public String getCountPercentage() {
+		return countPercentage;
+	}
+
+	public void setCountPercentage(String countPercentage) {
+		this.countPercentage = countPercentage;
+	}
+}
